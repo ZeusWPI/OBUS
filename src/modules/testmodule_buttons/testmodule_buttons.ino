36af43b1445588210b29a17c90e292029a884095
redfast00 <redfast00@gmail.com>
2020-08-26 23:36:35 +0200

    Add basic module with two buttons

---
+++ src/modules/testmodule_buttons/testmodule_buttons.ino
@@ -0,0 +1,44 @@
+// (c) 2020, redfast00
+// See the LICENSE file for conditions for copying
+
+#include <obus_module.h>
+#include <ezButton.h>
+
+ezButton red_button(5);
+ezButton green_button(6);
+
+void setup() {
+	Serial.begin(115200);
+	// WARNING: do not use 255 for your module
+	obus_module::setup(OBUS_TYPE_PUZZLE, 255);
+	red_button.setDebounceTime(100);
+	green_button.setDebounceTime(100);
+}
+
+obus_can::message message;
+
+void loop() {
+	bool received = obus_module::loop(&message);
+	// TODO handle update frames (not needed for this module, but could be useful as example code)
+
+	red_button.loop();
+	green_button.loop();
+
+	if (red_button.getCount() > 0) {
+		red_button.resetCount();
+		obus_module::strike();
+	}
+
+	if (green_button.getCount() > 0) {
+		green_button.resetCount();
+		obus_module::solve();
+	}
+}
+
+void callback_game_start() {
+	// Intentionally emtpy
+}
+
+void callback_game_stop() {
+	// Intentionally empty
+}
